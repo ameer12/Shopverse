c7bc312b80d4f94dbd6eb6348d52f6c37b5ffa11
ameer12 <fatenhs343@gmail.com>
2025-12-19 09:11:53 +0300

    Create Header.tsx

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="h-16 border-b border-gray-200 flex items-center px-6 bg-white shadow-sm">
+      <h2 className="text-lg font-semibold text-blue-800">Dashboard</h2>
+    </header>
+  );
+}
